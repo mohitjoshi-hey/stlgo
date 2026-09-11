6c2e0ecaa0e61668ba31b2b51161f58a54150eb3
Mohit Joshi <25btcs080@iiitu.ac.in>
2026-09-11 10:45:17 +0530

    updated deque.go

--- container/deque/deque.go
+++ container/deque/deque.go
@@ -1,35 +1,35 @@
 package deque
 
 type Deque[T any] struct {
-	val []T
-	head int
-	tail int
-	len int
+	val []T;
+	head int;
+	tail int;
+	len int;
 }
 
 func New[T any](items ...T) *Deque[T] {
 	capacity := len(items)
 	if capacity < 4 {
-		capacity = 4
+		capacity = 4;
 	}
 
 	d := &Deque[T]{
-		val: make([]T, capacity),
+		val: make([]T,capacity),
 		head: 0,
 		tail: len(items),
-		len: len(items),
+		len:len(items),
 	}
 
 	copy(d.val, items)
-	return d
+	return d;
 }
 
 func (d *Deque[T]) IsEmpty() bool { 
-return d.len == 0 
+ return d.len == 0;
 }
 
 func (d *Deque[T]) Len() int { 
-return d.len
+ return d.len;
 }
 
 func (d *Deque[T]) grow() {
@@ -44,20 +44,20 @@ func (d *Deque[T]) grow() {
 
 func (d *Deque[T]) PushBack(value T) {
 	if d.len == len(d.val) {
-		d.grow()
+	       d.grow();
 	}
 	d.val[d.tail] = value
-	d.tail = (d.tail + 1) % len(d.val)
-	d.len++
+	d.tail = (d.tail + 1) % len(d.val);
+	d.len++;
 }
 
 func (d *Deque[T]) PushFront(value T) {
 	if d.len == len(d.val) {
-		d.grow()
+		d.grow();
 	}
-	d.head = (d.head - 1 + len(d.val)) % len(d.val)
-	d.val[d.head] = value
-	d.len++
+	d.head = (d.head - 1 + len(d.val))% len(d.val)
+	d.val[d.head] = value;
+	d.len++;
 }
 
 func (d *Deque[T]) PopBack() (T, bool) {
@@ -66,24 +66,25 @@ func (d *Deque[T]) PopBack() (T, bool) {
 		return zero, false
 	}
 	capacity := len(d.val)
-	d.tail = (d.tail - 1 + capacity) % capacity
+	d.tail = (d.tail -1 + capacity) % capacity
 	ele := d.val[d.tail]
 	var zero T
 	d.val[d.tail] = zero
-	d.len--
+	d.len--;
 	return ele, true
 }
 
 func (d *Deque[T]) PopFront() (T, bool) {
 	if d.IsEmpty() {
 		var zero T
-		return zero, false
+		return zero, false,
 	}
 	ele := d.val[d.head]
 	var zero T
-	d.val[d.head] = zero
+	d.val[d.head] = zero:
+;
 	d.head = (d.head + 1) % len(d.val)
-	d.len--
+        d.len--
 	return ele, true
 }
 
